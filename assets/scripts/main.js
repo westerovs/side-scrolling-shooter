360d35770b3575b5e395ca317d385f69841003bd
Александр <westerovs@mail.ru>
2022-04-14 20:52:18 +0300

    create start bg

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,10 @@
-let config = {
+import StartScene from './scenes/StartScene.js'
+
+const config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
     scene: [StartScene]
-};
+}
 
-let game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
--- assets/scripts/scenes/StartScene.js
+++ assets/scripts/scenes/StartScene.js
@@ -1,14 +1,17 @@
-class StartScene extends Phaser.Scene {
-    constructor() {
-        super('Start');
-    }
-    preload() {
-        this.load.image('bg', 'assets/sprites/background.png');
-    }
-    create() {
-        this.createBackground();
-    }
-    createBackground() {
-        this.add.sprite(0, 0, 'bg').setOrigin(0);
-    }
+export default class StartScene extends Phaser.Scene {
+  constructor() {
+    super('Start') // параметр старт, в качестве названия этой сцены
+  }
+  
+  preload() {
+    this.load.image('bg', 'assets/sprites/background.png')
+  }
+  
+  create() {
+    this.createBackground()
+  }
+  
+  createBackground() {
+    this.add.sprite(0, 0, 'bg').setOrigin(0)
+  }
 }
